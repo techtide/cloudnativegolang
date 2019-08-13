5d35b982b455aa8a12132812dccfca2af52f3e43
unknown <armanbhalla6@gmail.com>
2019-08-13 12:03:47 +0100

    Changed pd to pb, added main.go for the service

---
+++ choose/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"godomicroservice/pb"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct{}
+
+func main() {
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("Cannot listen due to error :( %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterAddServiceServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Cannot serve service :( %v", err)
+	}
+}
